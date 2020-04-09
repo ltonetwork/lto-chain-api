159c0b7ba58b905ae66c04976aae1000c0fb8059
bbjansen <bobjansen@pm.me>
2020-04-09 14:55:17 +0200

    ready for v0.04 collector

--- routes/block.js
+++ routes/block.js
@@ -12,14 +12,16 @@ const db = require('../utils/utils').knex
 // Block last
 router.get('/last', async function (req, res, next) {
   try {
+
     const getBlock = await db('blocks')
-      .leftJoin('consensus', 'blocks.index', 'consensus.index')
-    // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-      .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(blocks.timestamp, "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      //.leftJoin('features', 'blocks.index', 'features.index')
+      .select()
       .orderBy('blocks.index', 'desc')
       .limit(1)
 
-    res.status(200).json(getBlock[0])
+    res.status(200).json(getBlock)
+    
   } catch (err) {
     console.log(err)
     next(err)
@@ -27,96 +29,83 @@ router.get('/last', async function (req, res, next) {
 })
 
 router.get('/last/:amount',
-  [
-    check('amount')
-      .not().isEmpty()
-      .isInt({
-        min: 1,
-        max: 1000
-      })
-  ],
-  validateInput,
-  async function (req, res, next) {
-    try {
-      const getBlocks = await db('blocks')
-        .leftJoin('consensus', 'blocks.index', 'consensus.index')
-      // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-        .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(blocks.timestamp, "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
-        .orderBy('blocks.index', 'desc')
-        .limit(req.params.amount)
+[
+  check('amount')
+    .not().isEmpty()
+    .isInt({
+      min: 1,
+      max: 1000
+    })
+],
+validateInput,
+async function (req, res, next) {
+  try {
+    const getBlocks = await db('blocks')
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      .select('blocks.index', 'blocks.reference', 'blocks.generator', 'blocks.signature', 'blocks.size', 'blocks.count', 'blocks.fee', 'blocks.version', 'blocks.timestamp', 'blocks.verified', 'consensus.signature', 'consensus.target')
+      .orderBy('blocks.index', 'desc')
+      .limit(req.params.amount)
 
-      res.status(200).json(getBlocks)
-    } catch (err) {
-      console.log(err)
-      next(err)
-    }
-  })
+    res.status(200).json(getBlocks)
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
+})
 
 // Get block by generator address
 router.get('/address/:address',
-  [
-    check('address')
-      .not().isEmpty()
-      .isLength({
-        min: 35,
-        max: 35
-      })
-  ],
-  validateInput,
-  async function (req, res, next) {
-    try {
-      const getBlock = await db('blocks')
-        .leftJoin('consensus', 'blocks.index', 'consensus.index')
-      // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-        .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(blocks.timestamp, "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
-        .where('generator', req.params.address)
-        .orderBy('blocks.index', 'desc')
+[
+  check('address')
+    .not().isEmpty()
+    .isLength({
+      min: 35,
+      max: 35
+    })
+],
+validateInput,
+async function (req, res, next) {
+  try {
+    const getBlock = await db('blocks')
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      .select('blocks.index', 'blocks.reference', 'blocks.generator', 'blocks.signature', 'blocks.size', 'blocks.count', 'blocks.fee', 'blocks.version', 'blocks.timestamp', 'blocks.verified', 'consensus.signature', 'consensus.target')
+      .where('generator', req.params.address)
+      .orderBy('blocks.index', 'desc')
 
-      res.status(200).json(getBlock)
-    } catch (err) {
-      next(err)
-    }
-  })
+    res.status(200).json(getBlock[0])
+  } catch (err) {
+    next(err)
+  }
+})
 
 // Block Range
 router.get('/:start/:end',
-  [
-    check('start')
-      .not().isEmpty()
-      .isInt({
-        min: 1
-      }),
+[
+  check('start').not().isEmpty().isInt({min: 1}),
+  check('end').not().isEmpty().isInt({min: 1})
+],
+validateInput,
+async function (req, res, next) {
+  try {
+    const getBlocks = await db('blocks')
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      .select('blocks.index', 'blocks.reference', 'blocks.generator', 'blocks.signature', 'blocks.size', 'blocks.count', 'blocks.fee', 'blocks.version', 'blocks.timestamp', 'blocks.verified', 'consensus.signature', 'consensus.target')
+      .whereBetween('blocks.index', [req.params.start, req.params.end])
+      .orderBy('blocks.index', 'desc')
+      .limit(1000)
 
-    check('end')
-      .not().isEmpty()
-      .isInt({
-        min: 1
-      })
-  ],
-  validateInput,
-  async function (req, res, next) {
-    try {
-      const getBlocks = await db('blocks')
-        .leftJoin('consensus', 'blocks.index', 'consensus.index')
-      // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-        .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(blocks.timestamp, "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
-        .whereBetween('blocks.index', [req.params.start, req.params.end])
-        .orderBy('blocks.index', 'desc')
-        .limit(1000)
-
-      res.status(200).json(getBlocks)
-    } catch (err) {
-      next(err)
-    }
-  })
+    res.status(200).json(getBlocks)
+  } catch (err) {
+    next(err)
+  }
+})
 
 // Get unverified blocks
 router.get('/unverified', async function (req, res, next) {
   try {
     const getBlocks = await db('blocks')
-      .leftJoin('consensus', 'blocks.index', 'consensus.index')
-    // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-      .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(from_unixtime(blocks.timestamp, "%Y-%m-%d"), "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      .select('blocks.index', 'blocks.reference', 'blocks.generator', 'blocks.signature', 'blocks.size', 'blocks.count', 'blocks.fee', 'blocks.version', 'blocks.timestamp', 'blocks.verified', 'consensus.signature', 'consensus.target')
       .whereRaw('blocks.timestamp > NOW() - INTERVAL 90 MINUTE')
       .where('blocks.verified', false)
       .orderBy('blocks.index', 'desc')
@@ -131,28 +120,23 @@ router.get('/unverified', async function (req, res, next) {
 
 // Block at index
 router.get('/:index',
-  [
-    check('index')
-      .not().isEmpty()
-      .isInt({
-        min: 1
-      })
-  ],
-  validateInput,
-  async function (req, res, next) {
-    try {
-      const getBlock = await db('blocks')
-        .leftJoin('consensus', 'blocks.index', 'consensus.index')
-      // .innerJoin('transactions', 'blocks.index', 'transactions.block')
-        .select(db.raw('blocks.index, blocks.reference, blocks.generator, blocks.signature, blocks.size, blocks.count, blocks.fee, blocks.version, from_unixtime(blocks.timestamp, "%Y-%m-%d"), blocks.verified, consensus.signature, consensus.target'))
-        .where('blocks.index', req.params.index)
-        .orderBy('blocks.index', 'desc')
-        .limit(1)
+[
+  check('index').not().isEmpty().isInt({min: 1})
+],
+validateInput,
+async function (req, res, next) {
+  try {
+    const getBlock = await db('blocks')
+      //.leftJoin('consensus', 'blocks.index', 'consensus.index')
+      .select('blocks.index', 'blocks.reference', 'blocks.generator', 'blocks.signature', 'blocks.size', 'blocks.count', 'blocks.fee', 'blocks.version', 'blocks.timestamp', 'blocks.verified', 'consensus.signature', 'consensus.target')
+      .where('blocks.index', req.params.index)
+      .orderBy('blocks.index', 'desc')
+      .limit(1)
 
-      res.status(200).json(getBlock[0])
-    } catch (err) {
-      next(err)
-    }
-  })
+    res.status(200).json(getBlock[0])
+  } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router
--- routes/stats.js
+++ routes/stats.js
@@ -10,300 +10,55 @@ const validateInput = require('../middleware/validateInput')
 const db = require('../utils/utils').knex
 const moment = require('moment')
 
-// stats
-router.get('/all', async function (req, res, next) {
-  try {
-    const firstBlock = await db('blocks')
-      .select('timestamp')
-      .where('index', 1)
-      .limit(1)
 
-    const lastBlock = await db('blocks')
-      .select('index', 'timestamp')
-      .orderBy('blocks.index', 'desc')
-      .limit(1)
-
-    const avgBlock = await db('blocks')
-      .avg('size as size')
-      .avg('count as count')
-      .avg('fee as fee')
-
-    const sumBlock = await db('blocks')
-      .sum('size as size')
-      .sum('count as count')
-      .sum('fee as fee')
-
-    const blockDiff = moment.duration(moment(lastBlock[0].timestamp).diff(moment(firstBlock[0].timestamp))).asSeconds()
-    const avgBlockTime = blockDiff / (lastBlock[0].index - 1)
-
-    const countTxns = await db('transactions')
-      .count('* as count')
-      .where('type', 4)
-
-    const countAnchors = await db('transactions')
-      .count('* as count')
-      .where('type', 15)
-
-    const countStartLease = await db('transactions')
-      .count('* as count')
-      .where('type', 8)
-
-    const countCancelLease = await db('transactions')
-      .count('* as count')
-      .where('type', 9)
-
-    const countMassTx = await db('transactions')
-      .count('* as count')
-      .where('type', 11)
-
-    const countMassTransfers = await db('transfers')
-      .count('* as count')
-
-    const totalAddresses = await db('addresses')
-      .count('* as count')
-
-    const activeAddresses = await db('addresses')
-      .count('* as count')
-      .where('regular', '>', 0)
-      .orWhere('generating', '>', 0)
-      .orWhere('available', '>', 0)
-      .orWhere('effective', '>', 0)
-
-    const totalGenerators = await db('blocks')
-      .count('generator as sum')
-      .groupBy('generator')
-
-    const avgConsensus = await db('consensus')
-      .avg('target as target')
-
-    const stats = {
-      stats: {
-        totalBlocks: lastBlock[0].index,
-        totalTxns: sumBlock[0].count,
-        totalSize: sumBlock[0].size,
-        totalFee: sumBlock[0].fee,
-        totalAddresses: totalAddresses[0].count,
-        totalGenerators: totalGenerators.length,
-        averageEmission: +avgBlockTime.toFixed(0),
-        averageBlockSize: +avgBlock[0].size.toFixed(0),
-        averageBlockTxns: +avgBlock[0].count.toFixed(2),
-        averageBlockFee: +avgBlock[0].fee.toFixed(2),
-        averageTarget: +avgConsensus[0].target.toFixed(2),
-        activeLeases: countStartLease[0].count - countCancelLease[0].count,
-        activeAddresses: activeAddresses[0].count,
-        tx: {
-          standard: countTxns[0].count,
-          anchor: countAnchors[0].count,
-          massTransactions: countMassTx[0].count,
-          massTransfers: countMassTransfers[0].count,
-          startLease: countStartLease[0].count,
-          cancelLease: countCancelLease[0].count
-        }
-      },
-      timestamp: moment().format('YYYY-MM-DD HH:mm:ss Z')
-    }
-    res.status(200).json(stats)
-  } catch (err) {
-    next(err)
-  }
-})
-
-// Get transactions count by period
-router.get('/transaction/:period',
+// Get transactions count between two dates
+router.get('/transaction/:start/:end',
 [
-  check('period')
-    .not().isEmpty()
+  check('start').not().isEmpty(),
+  check('end').not().isEmpty()
 ],
 validateInput,
 async function (req, res, next) {
   try {
-    let range
-    let scale
 
-    if (req.params.period === 'day') {
-      range = 'day'
-      scale = '%Y-%m-%d %H'
-    } else if (req.params.period === 'week') {
-      range = 'week'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'month') {
-      range = 'month'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'year') {
-      range = 'year'
-      scale = '%Y-%m'
+    const start = moment(req.params.start, 'x')
+    const end = moment(req.params.end, 'x')
+
+    // Let's figure out the range
+    let format
+    const difference = moment(end).diff(moment(start), 'days')
+
+    if (difference <= '1') { // Day range -> hour
+
+      format = '%Y-%m-%d %H:00:00'
+    }
+    else if ((difference >= 1) && (difference <= 7)) { // Week range -> day
+
+      format = '%Y-%m-%d 00:00:00'
+    }
+    else if ((difference > 7) && (difference <= moment().daysInMonth()))  { // Month range -> day
+      
+      format = '%Y-%m-%d 00:00:00'
+    }
+    else if ((difference > moment().daysInMonth()) && (difference <= 366))  { // Year range -> month
+      
+      format = '%Y-%m-00 00:00:00'
+    }
+    else { // Multiple Years early
+
+      format = '%Y-%m-00 00:00:00'
     }
 
-      const getData = await db('transactions')
-      .select(db.raw('from_unixtime(timestamp, "' + scale + '") as period'))
-      .count('id as count')
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-      .groupByRaw('from_unixtime(timestamp, "' + scale + '")')
+    console.log(format)
 
-    res.status(200).json(getData)
-  } catch (err) {
-    next(err)
-  }
-})
+    const data = await db('transactions')
+    //.select(db.raw(range + '(from_unixtime(timestamp / 1000)) as date'))
+    .select(db.raw('date_format(from_unixtime(timestamp / 1000), "' + format + '") as period'))
+    .count('id as count')
+    .whereBetween('timestamp', [req.params.start, req.params.end])
+    .groupByRaw('period')
 
-// Get transactions count by period
-router.get('/transaction/type/:period',
-[
-  check('period')
-    .not().isEmpty()
-],
-validateInput,
-async function (req, res, next) {
-  try {
-    let range
-    let scale
-
-    if (req.params.period === 'day') {
-      range = 'day'
-      scale = '%Y-%m-%d %H'
-    } else if (req.params.period === 'week') {
-      range = 'week'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'month') {
-      range = 'month'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'year') {
-      range = 'year'
-      scale = '%Y-%m'
-    }
-
-      const getData = await db('transactions')
-      .select(db.raw('from_unixtime(timestamp, "' + scale + '") as period, type'))
-      .count('id as count')
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-      .groupByRaw('from_unixtime(timestamp, "' + scale + '")')
-      .groupBy('type')
-
-    res.status(200).json(getData)
-  } catch (err) {
-    next(err)
-  }
-})
-
-
-// Get detailed transactions count by period
-router.get('/transaction/detailed/:period',
-[
-  check('period')
-    .not().isEmpty()
-],
-validateInput,
-async function (req, res, next) {
-  try {
-    let range
-    let scale
-
-    if (req.params.period === 'day') {
-      range = 'day'
-      scale = '%Y-%m-%d %H'
-    } else if (req.params.period === 'week') {
-      range = 'week'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'month') {
-      range = 'month'
-      scale = '%Y-%m-%d'
-    } else if (req.params.period === 'year') {
-      range = 'year'
-      scale = '%Y-%m'
-    }
-
-    const countTxns = await db('transactions')
-      .count('* as count')
-      .where('type', 4)
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-
-    const countAnchors = await db('transactions')
-      .count('* as count')
-      .where('type', 15)
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-
-    const countStartLease = await db('transactions')
-      .count('* as count')
-      .where('type', 8)
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-
-    const countCancelLease = await db('transactions')
-      .count('* as count')
-      .where('type', 9)
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-
-    const countMassTx = await db('transactions')
-      .count('* as count')
-      .select()
-      .where('type', 11)
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-
-    const countMassTransfers = await db('transfers')
-      .leftOuterJoin('transactions', 'transfers.tid', 'transactions.id')
-      .count('* as count')
-      .whereBetween('transactions.timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-    // .groupBy('transfers.tid')
-
-    const getData = await db('transactions')
-      .select(db.raw('from_unixtime(timestamp, "' + scale + '") as period, type'))
-      .count('* as count')
-      .whereBetween('timestamp', [moment().subtract(1, range).format('YYYY-MM-DD HH:mm:ss'), moment().format('YYYY-MM-DD HH:mm:ss')])
-      .groupByRaw('from_unixtime(timestamp, "' + scale + '")')
-      .groupBy('type')
-
-    // Sort data by period and type, thanks IBMC <3
-    const temp = {}
-
-    getData.forEach((obj) => {
-      if (!temp[obj.period]) temp[obj.period] = {}
-      if (!temp[obj.period][obj.type]) temp[obj.period][obj.type] = 0
-      temp[obj.period][obj.type] += obj.count
-    })
-
-    const data = []
-    Object.keys(temp).forEach((key) => {
-    // Populate data with 0 for missing data
-      if (!temp[key][4]) {
-        temp[key][4] = 0
-      }
-
-      if (!temp[key][11]) {
-        temp[key][11] = 0
-      }
-
-      if (!temp[key][15]) {
-        temp[key][15] = 0
-      }
-
-      if (!temp[key][8]) {
-        temp[key][8] = 0
-      }
-
-      if (!temp[key][9]) {
-        temp[key][9] = 0
-      }
-
-      data.push({
-        period: key,
-        types: temp[key]
-      })
-    })
-
-    // console.log(data)
-    const stats = {
-      stats: {
-        standard: countTxns[0].count,
-        anchor: countAnchors[0].count,
-        massTransactions: countMassTx[0].count,
-        massTransfers: countMassTransfers[0].count,
-        startLease: countStartLease[0].count,
-        cancelLease: countCancelLease[0].count
-      },
-      data: data,
-      timestamp: moment().format('YYYY-MM-DD HH:mm:ss Z')
-    }
-
-    res.status(200).json(stats)
+    res.status(200).json(data)
   } catch (err) {
     next(err)
   }
