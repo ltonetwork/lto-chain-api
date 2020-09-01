c6cd245ce5ec161869449b60d69103bd624295b7
bbjansen <bobjansen@pm.me>
2020-09-01 14:04:35 +0200

    fix json parse

--- routes/generator.js
+++ routes/generator.js
@@ -1,102 +1,104 @@
 // Copyright (c) 2018-2019, BB Jansen
 //
 // Please see the included LICENSE file for more information.
-'use strict'
+"use strict";
 
-const express = require('express')
-const router = express.Router()
-const { check } = require('express-validator/check')
-const validateInput = require('../middleware/validateInput')
-const db = require('../utils/utils').knex
-const moment = require('moment')
-const axios = require('axios')
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator/check");
+const validateInput = require("../middleware/validateInput");
+const db = require("../utils/utils").knex;
+const moment = require("moment");
+const axios = require("axios");
 
 // Get generators stats total
-router.get('/all', async function (req, res, next) {
+router.get("/all", async function (req, res, next) {
   try {
-    const generators = await db('blocks')
-      .leftJoin('addresses', 'blocks.generator', 'addresses.address')
-      .select('blocks.generator', 'addresses.generating as pool', 'addresses.label', 'addresses.url')
-      .count('blocks.index as blocks')
-      .sum('blocks.fee as earnings')
-      .groupBy('blocks.generator')
-      .orderBy('blocks', 'desc')
+    const generators = await db("blocks")
+      .leftJoin("addresses", "blocks.generator", "addresses.address")
+      .select(
+        "blocks.generator",
+        "addresses.generating as pool",
+        "addresses.label",
+        "addresses.url"
+      )
+      .count("blocks.index as blocks")
+      .sum("blocks.fee as earnings")
+      .groupBy("blocks.generator")
+      .orderBy("blocks", "desc");
 
     let total = generators.reduce((blocks, generator, index, generators) => {
-      return blocks += generator.blocks
-    }, 0)
+      return (blocks += generator.blocks);
+    }, 0);
 
-    generators.forEach(generator => {
-      generator.share = +(generator.blocks / total).toFixed(5) || 0
-    })
+    generators.forEach((generator) => {
+      generator.share = +(generator.blocks / total).toFixed(5) || 0;
+    });
 
-    res.status(200).json(generators)
+    res.status(200).json(generators);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // Get generators stats (day)
-router.get('/all/:start/:end',
-[
-  check('start').not().isEmpty(),
-  check('end').not().isEmpty(),
+router.get(
+  "/all/:start/:end",
+  [check("start").not().isEmpty(), check("end").not().isEmpty()],
+  validateInput,
+  async function (req, res, next) {
+    try {
+      const generators = await db("blocks")
+        .leftJoin("addresses", "blocks.generator", "addresses.address")
+        .select(
+          "blocks.generator",
+          "addresses.generating as pool",
+          "addresses.label",
+          "addresses.url"
+        )
+        .count("blocks.index as blocks")
+        .sum("blocks.fee as earnings")
+        .whereBetween("blocks.timestamp", [req.params.start, req.params.end])
+        .groupBy("blocks.generator")
+        .orderBy("blocks", "desc");
 
-],
-validateInput,
-async function (req, res, next) {
-  try {
+      let total = generators.reduce((blocks, generator, index, generators) => {
+        return (blocks += generator.blocks);
+      }, 0);
 
-    const generators = await db('blocks')
-      .leftJoin('addresses', 'blocks.generator', 'addresses.address')
-      .select('blocks.generator', 'addresses.generating as pool', 'addresses.label', 'addresses.url')
-      .count('blocks.index as blocks')
-      .sum('blocks.fee as earnings')
-      .whereBetween('blocks.timestamp', [req.params.start, req.params.end])
-      .groupBy('blocks.generator')
-      .orderBy('blocks', 'desc')
+      generators.forEach((generator) => {
+        generator.share = +((generator.blocks / total) * 100).toFixed(5) || 0;
+      });
 
-    let total = generators.reduce((blocks, generator, index, generators) => {
-      return blocks += generator.blocks
-    }, 0)
-
-    generators.forEach(generator => {
-      generator.share = +((generator.blocks / total) * 100).toFixed(5) || 0
-    })
-
-    res.status(200).json(generators)
-  } catch (err) {
-    next(err)
+      res.status(200).end(generators);
+    } catch (err) {
+      next(err);
+    }
   }
-})
-
+);
 
 // Get generators stats (weekly)  from lto.tools
-router.get('/staking/weekly',
-validateInput,
-async function (req, res, next) {
+router.get("/staking/weekly", validateInput, async function (req, res, next) {
   try {
-    const staking = await axios.get('https://lto.tools/generators-weekly/json')
+    const staking = await axios.get("https://lto.tools/generators-weekly/json");
 
-    res.status(200).json(staking.data)
+    res.status(200).end(staking.data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
-
+});
 
 // Get generators stats (monthly)  from lto.tools
-router.get('/staking/monthly',
-validateInput,
-async function (req, res, next) {
+router.get("/staking/monthly", validateInput, async function (req, res, next) {
   try {
-    const staking = await axios.get('https://lto.tools/generators-monthly/json')
+    const staking = await axios.get(
+      "https://lto.tools/generators-monthly/json"
+    );
 
-    res.status(200).json(staking.data)
+    res.status(200).end(staking.data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-
-module.exports = router
+module.exports = router;
